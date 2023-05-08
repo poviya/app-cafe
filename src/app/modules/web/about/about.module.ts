2f0286f7e2f27114ef98556e52b468615d130a04
juliacode21 <felipecruzjulia74@gmail.com>
2023-05-08 02:43:03 -0400

    hab

--- src/app/modules/web/about/about.module.ts
+++ src/app/modules/web/about/about.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AboutRoutingModule } from './about-routing.module';
 import { MainComponent } from './pages/main/main.component';
 import { HistoryComponent } from './pages/history/history.component';
+import { VisionComponent } from './pages/vision/vision.component';
 
 
 @NgModule({
   declarations: [
     MainComponent,
-    HistoryComponent
+    HistoryComponent,
+    VisionComponent,
   ],
   imports: [
     CommonModule,
